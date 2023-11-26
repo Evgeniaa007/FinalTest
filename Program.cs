734cc90fb6084f2bcc21ced09fe3782dc2cff167
Evgeniia <jonnydor@gmail.com>
2023-11-26 14:26:08 +0300

    Создан массив типа string

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+string[] CreateStrArray(int size)
+{
+    string[] StringArray = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        StringArray[i] = Console.ReadLine();
+    }
+    return StringArray;
+}
+
+void PrintArray(string[] StrArray)
+{
+    Console.Write($"String array: ");
+    for (int i = 0; i < StrArray.Length; i++)
+    {
+        Console.Write($"{StrArray[i]}, ");
+    }
+    Console.Write($"\b\b ");
+}
+
+Console.WriteLine($"Input string array size: ");
+int N = Convert.ToInt32(Console.ReadLine());
+PrintArray(CreateStrArray(N));
