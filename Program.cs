6dbb0a776aaf6ff018e0c84ac906bdf719d3d586
Evgeniia <jonnydor@gmail.com>
2023-11-26 15:22:04 +0300

    Добавлена функция по подсчету размера элементов массива, которая оставляет в массиве только элементы, состоящие из 3х символов

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection.Metadata.Ecma335;
+﻿using System.Dynamic;
+using System.Reflection.Metadata.Ecma335;
 
 string[] CreateStrArray(int size)
 {
@@ -15,11 +16,32 @@ void PrintArray(string[] StrArray)
     Console.Write($"String array: ");
     for (int i = 0; i < StrArray.Length; i++)
     {
-        Console.Write($"{StrArray[i]}, ");
+        Console.Write($"{StrArray[i]} ");
     }
-    Console.Write($"\b\b ");
 }
 
 Console.WriteLine($"Input string array size: ");
 int N = Convert.ToInt32(Console.ReadLine());
-PrintArray(CreateStrArray(N));
+string[] NewArray = CreateStrArray(N);
+Console.WriteLine();
+PrintArray(NewArray);
+Console.WriteLine();
+
+string[] SecondStrArray(string[] array)
+{
+    string[] array2 = new string[array.Length];
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            array2[count] = array[i];
+            count++;
+        }
+    }
+    return array2;
+}
+
+string[] finalArray = SecondStrArray(NewArray);
+Console.WriteLine("The new array which elements consist of 3 symbols: ");
+PrintArray(finalArray);
